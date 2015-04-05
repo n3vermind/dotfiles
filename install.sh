313184ba48e639a88f751f2c696bee3f41d57a0f
n3v <thezbylut@gmail.com>
2015-04-06 00:43:11 +0200

    Installing youcompleteme.

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+git submodule update --init --recursive
+
 ln -s ~/.dotfiles/xinitrc ~/.xinitrc
 ln -s ~/.dotfiles/Xresource ~/.Xresource
 
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
+(cd ./vim/bundle/youcompleteme && ./install.sh --clang-completer --system-libclang)
 
 mkdir -p ~/.config
 ln -s ~/.dotfiles/awesome ~/.config/awesome
