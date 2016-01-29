c04a741cfcf10dd0d3adbadea5825a178c4ecfe0
n3v <thezbylut@gmail.com>
2016-01-29 18:43:45 +0100

    Cleaned up installation script.

--- install.sh
+++ install.sh
@@ -1,18 +1,51 @@
 #!/bin/bash
 
+set -e
+
+function link {
+  SOURCE=$1
+  DEST=$2
+
+  if [ -L $DEST ]; then
+    unlink $DEST
+  fi
+
+  if [ -f $DEST ]; then
+    echo "$DEST is a regular file"
+  elif [ -d $DEST ]; then
+    echo "$DEST is a directory"
+  else
+    ln -s $SOURCE $DEST
+    echo "linked $SOURCE to $DEST"
+  fi
+}
+
+function install_youcompleteme {
+  YCMPATH=$DIR/vim/bundle/YouCompleteMe
+  if [ -f $YCMPATH/third_party/ycmd/ycm_core.so ]; then
+    echo "YouCompleteMe already installed"
+  else
+    $(cd $YCMPATH && ./install.sh --clang-completer --system-libclang --gocode-completer)
+  fi
+}
+
+DIR=$(dirname "$(readlink -f "$0")")
+
+cd $DIR
+
 git submodule update --init --recursive
 
-ln -s ~/.dotfiles/xinitrc ~/.xinitrc
-ln -s ~/.dotfiles/Xresources ~/.Xresources
+link $DIR/xinitrc $HOME/.xinitrc
+link $DIR/Xresources $HOME/.Xresources
 
-ln -s ~/.dotfiles/vim ~/.vim
-ln -s ~/.dotfiles/vimrc ~/.vimrc
+link $DIR/vim $HOME/.vim
+link $DIR/vimrc $HOME/.vimrc
 vim +PluginInstall +qall
-(cd ./vim/bundle/youcompleteme && ./install.sh --clang-completer --system-libclang)
+install_youcompleteme
 
 mkdir -p ~/.config
-ln -s ~/.dotfiles/awesome ~/.config/awesome
+link $DIR/awesome $HOME/.config/awesome
 
-ln -s ~/.dotfiles/zshrc ~/.zshrc
+link $DIR/zshrc $HOME/.zshrc
 
-ln -s ~/.dotfiles/gdbinit ~/.gdbinit
+link $DIR/gdbinit $HOME/.gdbinit
