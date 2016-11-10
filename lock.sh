8f40bf81cbd16b476054332168b6122d8468e4d0
n3v <thezbylut@gmail.com>
2016-11-10 23:32:18 +0100

    Update configs to newer i3lock.

--- lock.sh
+++ lock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 bgfile="$(mktemp --suffix=.png)"
-locker="i3lock -c 000000 -i $bgfile -d"
+locker="i3lock -c 000000 -i $bgfile -n"
 
 # Delay in seconds. Note that by default systemd-logind allows a maximum sleep
 # delay of 5 seconds.
@@ -11,12 +11,14 @@ sleep_delay=1
 pre_lock() {
   scrot "$bgfile"
   convert "$bgfile" -scale 10% -scale 1000% -blur 0x5 "$bgfile"
+  xset dpms 5 5 5
   return
 }
 
 # Run after the locker exits
 post_lock() {
   rm "$bgfile"
+  xset dpms 0 0 0
   return
 }
 
@@ -27,7 +29,7 @@ trap 'kill %%' TERM INT
 
 if [[ -e /dev/fd/${XSS_SLEEP_LOCK_FD:--1} ]]; then
     # lock fd is open, make sure the locker does not inherit a copy
-    $locker {XSS_SLEEP_LOCK_FD}<&- &
+    $locker &
 
     sleep $sleep_delay
 
